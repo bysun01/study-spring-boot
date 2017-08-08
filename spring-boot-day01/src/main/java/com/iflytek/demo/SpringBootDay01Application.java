7b1e19391a665b06e00f62e8e0559b5c41093847
bysun01 <452284516@qq.com>
2017-08-09 00:39:11 +0800

    create

---
+++ spring-boot-day01/src/main/java/com/iflytek/demo/SpringBootDay01Application.java
@@ -0,0 +1,12 @@
+package com.iflytek.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDay01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDay01Application.class, args);
+	}
+}
